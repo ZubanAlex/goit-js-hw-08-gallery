1ce4e4bdbe2818c671a6082486a6ba4a954a9fd6
ZubanAlex <zubanalex31@gmail.com>
2019-09-24 23:28:02 +0300

    fix modul 08 rev_01

--- js/script.js
+++ js/script.js
@@ -4,41 +4,37 @@ import galleryItems from './gallery-items.js';
 
 const galleryList = document.querySelector('.gallery');
 
-galleryItems.map(item => {
-  const createLi = document.createElement('li');
-  createLi.classList.add('gallery__item');
-  createLi.insertAdjacentHTML(
-    'afterbegin',
-    ` <a
-  class="gallery__link"
-  href="${item.original}"
-  >
-  <img
-  class="gallery__image"
-  src="${item.preview}"
-  data-source="${item.original}"
-  alt="${item.description}"
-  />
+const createListItems = galleryItems.reduce((acc, item) => {
+  acc += ` <li class='gallery__item'> <a
+     class="gallery__link"
+     href="${item.original}"
+     >
+     <img
+     class="gallery__image"
+     src="${item.preview}"
+     data-source="${item.original}"
+     alt="${item.description}"
+     />
   
-  <span class="gallery__icon">
-  <i class="material-icons">zoom_out_map</i>
-  </span>
-  </a>`,
-  );
-  galleryList.appendChild(createLi);
-});
+     <span class="gallery__icon">
+     <i class="material-icons">zoom_out_map</i>
+     </span>
+     </a></li>`;
+  return acc;
+}, '');
+
+galleryList.innerHTML = createListItems;
 
 const lightbox = document.querySelector('.lightbox');
 const lightboxImage = document.querySelector('.lightbox___image');
 const lightboxContent = document.querySelector('.lightbox__content');
-const linkOpenLightbox = document.querySelector('.gallery');
 const closeLightboxBtn = document.querySelector(
   'button[data-action="close-lightbox"]',
 );
 
 const openLightbox = function(event) {
   const galleryLink = Array.from(document.querySelectorAll('.gallery__link'));
-  galleryLink.map(function(item) {
+  galleryLink.forEach(item => {
     if (
       event.target.getAttribute('data-source') === item.getAttribute('href')
     ) {
@@ -57,11 +53,15 @@ const closeLightbox = function(event) {
   lightboxImage.setAttribute('alt', '');
 };
 
-linkOpenLightbox.addEventListener('click', openLightbox);
-closeLightboxBtn.addEventListener('click', closeLightbox);
-lightboxContent.addEventListener('click', function(event) {
+const closeLightboxWhitUsingOverlay = function(event) {
   if (event.target === this) closeLightbox();
-});
-window.addEventListener('keydown', function(event) {
+};
+
+const closeLightboxWhitUsingEsc = function(event) {
   if (event.keyCode === 27) closeLightbox();
-});
+};
+
+galleryList.addEventListener('click', openLightbox);
+closeLightboxBtn.addEventListener('click', closeLightbox);
+lightboxContent.addEventListener('click', closeLightboxWhitUsingOverlay);
+window.addEventListener('keydown', closeLightboxWhitUsingEsc);
